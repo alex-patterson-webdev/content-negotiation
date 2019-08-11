c13290222d60e1d3e40eb47773231d5a242386ab
Alex Patterson <alex.patterson.webdev@gmail.com>
2019-08-11 20:14:03 +0000

    Added Zend\Json as dev dependency and updated composer.

--- test/Service/JsonTest.php
+++ test/Service/JsonTest.php
@@ -1,9 +1,31 @@
 <?php
 
-
 namespace ArpTest\ContentNegotiation\Service;
 
-class JsonTest
+use Arp\ContentNegotiation\Service\ContentTypeHandlerInterface;
+use Arp\ContentNegotiation\Service\Json;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * JsonTest
+ *
+ * @author  Alex Patterson <alex.patterson.webdev@gmail.com>
+ * @package ArpTest\ContentNegotiation\Service
+ */
+class JsonTest extends TestCase
 {
+    /**
+     * testImplementsContentTypeHandlerInterface
+     *
+     * Ensure the class implements ContentTypeHandlerInterface.
+     *
+     * @test
+     */
+    public function testImplementsContentTypeHandlerInterface()
+    {
+        $handler = new Json();
+
+        $this->assertInstanceOf(ContentTypeHandlerInterface::class, $handler);
+    }
 
 }
